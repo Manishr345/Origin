ceddfb9074d364f65516db4c1e6a9fc94fa62c45
Manish345 <manishr030405@gmail.com>
2024-02-07 11:54:35 -0500

    funtion added for adjoint of 2x2 matrix

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-#include "C:\Users\DELL\Desktop\ORIGIN\Origin\matcom.h"
+#include "matcom.h"
 int main(){
     int a[3][3], b[3][3], c[3][3], c2[2][2], a2[2][2], b2[2][2], option;
-    printf("1. Inverse of 3x3 Matrix\n2.Deteminant of 2x2 Matrix\n3.Determinant of 3x3 Matrix\n4.Multiplication of 2x2 Matrices\n5.Multiplication of 3x3 Matrices\n6.Addition of 3x3 Matrices\n7.Subtraction of 3x3 Matrices\n8.Adjoint of 3x3 Matrix\n\nChoose one of the options above: ");
+    printf("1. Inverse of 3x3 Matrix\n2.Deteminant of 2x2 Matrix\n3.Determinant of 3x3 Matrix\n4.Multiplication of 2x2 Matrices\n5.Multiplication of 3x3 Matrices\n6.Addition of 3x3 Matrices\n7.Subtraction of 3x3 Matrices\n8.Adjoint of 3x3 Matrix\n9.Adjoint of 2x2 Matrix\nChoose one of the options above: ");
     scanf("%d", &option);
     switch(option){
         case 1:
@@ -113,6 +113,16 @@ int main(){
         printf("Adjoint: \n");
         adjoint3(a);
         break;
+
+	case 9:
+	printf("Enter the elements of Matrix: \n");
+	for(int i = 0; i < 2; i ++){
+		for(int j = 0; j < 2; j ++){
+			scanf("%d", &a2[i][j]);
+		}
+	}
+	adjoint2(a2);
+	break;
     }
     return 0;
 }
